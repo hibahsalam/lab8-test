18922429539b0d83c23e86f2d5ca84ab83399147
hibahsalam@yahoo.com <prakhar7799@gmail.com>
2020-10-15 18:05:38 +0530

    commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoDetailsComponent } from './todo-details/todo-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoListComponent,
+    TodoDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/predef-todo.ts
@@ -0,0 +1,7 @@
+import {Todo} from './todo';
+export const Todos: Todo[] = [
+{ id : 1, date : '12/1/2020', title : 'Assignment', description : 'to do assignment' },
+{ id : 2, date : '12/2/2020', title : 'Gym', description : 'to go to gym'},
+{ id : 3, date : '12/3/2020', title : 'Swimming', description : 'to go swimming'},
+{ id : 4, date : '12/4/2020', title : 'Gardening', description : 'to do gardening'}
+];
